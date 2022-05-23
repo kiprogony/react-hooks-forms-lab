5f26389a89b2b0bf79c112c3ccb7ffd9806ec158
Daryl Rogony <darylrogony@gmail.com>
2022-05-23 04:50:12 +0300

    Completed Assif=gnment

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ search, onSearchChange, onCategoryChange }) {
+  function searchChange(event) {
+    onSearchChange(event.target.value);
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..." 
+        value={search}
+        onChange={searchChange}
+       />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSumbit }) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("produce")
+
+  function nameChange(event) {
+    setName(event.target.value);
+  }
+
+  function categoryChange(event) {
+    setCategory(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onItemFormSumbit({
+      id: uuid(),
+      name,
+      category,
+    });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+         type="text"
+         name="name" 
+         value={name} 
+         onChange={nameChange}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select
+         name="category"
+         value={category}
+         onChange={categoryChange}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
